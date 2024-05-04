87326831298a057d6a4a645cd6c9b2381d555407
EmmaTheDev01 <worldoffictionrw@gmail.com>
2024-05-04 14:44:15 +0300

    Adding a different front page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Dashboard from './pages/Dashboard';
 import Home from './pages/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from './pages/Register';
+import FrontPage from './pages/FrontPage';
 function App() {
   return (
     <Router>
       <div className="App">
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/home' element={<FrontPage/>} />
         <Route path='/dashboard' element={<Dashboard />} />
         <Route path='/new-account' element={<Register />} />
       </Routes>
